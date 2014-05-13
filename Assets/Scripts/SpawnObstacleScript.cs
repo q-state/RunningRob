7060ce4255e920430cda6f3113f9398f4a7261cd
farhan78 <cold_fusion78@hotmail.com>
2014-05-13 16:23:29 +0100

    games files

---
+++ Assets/Scripts/SpawnObstacleScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObstacleScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMin = 1f;
+	public float spawnMax = 3f;
+	private GameObject currentItem;
+	private GameObject newItem;
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		currentItem = obj [Random.Range (0, obj.GetLength (0))];
+		Instantiate (currentItem, transform.position, Quaternion.identity);
+		//currentItem = item;
+	}
+	
+	void Update()
+	{
+		var distanceFromPlayer = currentItem.transform.position.x - player.position.x;
+		if (distanceFromPlayer <= 10f) {
+			newItem = obj [Random.Range (0, obj.GetLength (0))];
+			newItem.transform.position = new Vector3(currentItem.transform.position.x + 20f,currentItem.transform.position.y,0f);
+			Instantiate (newItem, transform.position, Quaternion.identity);
+			currentItem = newItem;
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMin = 1f;
+	public float spawnMax = 3f;
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+
+	void Spawn()
+	{
+		var item = obj [Random.Range (0, obj.GetLength (0))];
+		if (item.tag == "Obstacle") {
+			Instantiate (item, transform.position, Quaternion.identity);
+					
+				} else {
+			Instantiate (item, transform.position, Quaternion.identity);
+				}
+		Invoke ("Spawn", 1.3f);
+	}
+}
